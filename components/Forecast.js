008c87883638e923bb352535fce1d6190c2d626e
Azizun Ussama <estro.zun@gmail.com>
2020-08-30 00:58:44 +0700

    Update Forecast.js

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { _View } from 'react-native'
 
 export default function Forecast(props){
     return(
-        
+        <View>
+            <Text>{props.name}</Text>
+            <Text>{props.description}</Text>
+            <Text>{props.temp}</Text>
+            <Text>°C</Text>
+        </View>
     )
 }

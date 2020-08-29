5b61fc8926c6e643812721d87d5c967f31222f30
Azizun Ussama <estro.zun@gmail.com>
2020-08-29 23:47:50 +0700

    Update App.js

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () =>{
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>This app from visual studio</Text>
       <StatusBar style="auto" />
     </View>
   );
